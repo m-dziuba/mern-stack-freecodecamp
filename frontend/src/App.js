d8048c1fbada4e98f3046c4979077832b2888ce3
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2021-11-23 10:42:17 +0100

    Final commit to save finished project

---
+++ frontend/src/App.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import AddReview from "./components/addReview";
+import Restaurant from "./components/restaurants";
+import RestaurantsList from "./components/restaurantsList";
+import Login from "./components/login";
+
+function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurants" className="navbar-brand">
+          Restaurant Reviews
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item" key="1">
+            <Link to={"/restaurants"} className="nav-link">
+              Restaurants
+            </Link>
+          </li>
+          <li className="nav-item" key="2">
+            {user ? (
+              <a
+                onClick={logout}
+                className="nav-link"
+                style={{ cursor: "pointer" }}
+              >
+                Logout {user.name}
+              </a>
+            ) : (
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            )}
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route
+            exact
+            path={["/", "/restaurants"]}
+            component={RestaurantsList}
+          />
+          <Route
+            path="/restaurants/:id/review"
+            render={(props) => <AddReview {...props} user={user} />}
+          />
+          <Route
+            path="/restaurants/:id"
+            render={(props) => <Restaurant {...props} user={user} />}
+          />
+          <Route
+            path="/login"
+            render={(props) => <Login {...props} login={login} />}
+          />
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
